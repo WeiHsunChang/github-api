b950bb6253a7b6dad59b571ad06f17c8ea68a5ee
WeiHsunChang <weihsun.code@gmail.com>
2020-03-08 18:38:24 +0800

    revise

--- search-repos.js
+++ search-repos.js
@@ -1,10 +1,13 @@
 const fetch = require('node-fetch')
 const express = require('express');
+const search_url = require('./url')
 const app = express();
+
+let resData = {}
 
 function checkStatus(res) {
   if (res.ok) {
-    return res.json();
+    return res.json()
   } else {
     throw {
       error: res.statusText,
@@ -13,22 +16,31 @@ function checkStatus(res) {
   }
 }
 
-app.get('/search/:keyword', async (req, res) => {
-  let keyword = req.params.keyword;
-  let resData = {}
+function normalize(obj){
+  const data = obj.data || {}
+  const error = obj.error || 'non-error'
+  const errorCode = obj.errorCode || 200
+  return {
+    data : data,
+    error : error,
+    errorCode : errorCode
+  }
+}
 
-  await fetch('https://api.github.com/search/repositories?q=' + keyword)
-    .then(checkStatus)
-    .then(json => {
-      resData.data = json
-    })
-    .catch(err => {
+app.get('/search/:keyword', async (req, res) => {
+  const keyword = req.params.keyword
+  try{
+    const data = await fetch( search_url + keyword)
+    const json = await checkStatus(data)
+    resData.data = json
+  }
+  catch(err){
       resData.error = err.error
       resData.errorCode = err.errorCode
-    })
-  res.send(resData)
+  }
+  res.send(normalize(resData))
 });
 
-app.listen(3000, () => {
-  console.log('Listening');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening')
 });
---
+++ url.js
@@ -0,0 +1,2 @@
+const search_url = 'https://api.github.com/search/repositories?q='
+module.exports = search_url;
